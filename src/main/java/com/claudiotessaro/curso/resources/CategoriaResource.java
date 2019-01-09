f84b2c2ac6b79a213178ee8649196498995ce1dc
Claudio Tessaro <claudio0190@hotmail.com>
2019-01-08 22:50:43 -0300

    Categoria dominio criada

--- src/main/java/com/claudiotessaro/curso/resources/CategoriaResource.java
+++ src/main/java/com/claudiotessaro/curso/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.claudiotessaro.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.claudiotessaro.curso.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,17 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria (1,"Informatica");
+		Categoria cat2 = new Categoria(2, "Escrutirua");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
